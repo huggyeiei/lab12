f8bffffa83a89f7f7c950db5733cb5c0373a2dbd
huggyeiei <huggy2560@gmail.com>
2024-12-27 14:11:07 +0700

    success

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,22 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <ctime>
 using namespace std;
 
-int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+vector<long long> memo(51, -1); 
+
+long long fibonacci(int x) {
+    if (x == 0) return 0;
+    if (x == 1) return 1;
+    if (memo[x] != -1) return memo[x]; 
+    return memo[x] = fibonacci(x - 1) + fibonacci(x - 2);
+}
+
+int main() {
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
